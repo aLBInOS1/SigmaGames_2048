c05d44f53b61ec835588495b6cdcffed80e7f99c
aLBInOS1 <79748635+aLBInOS1@users.noreply.github.com>
2024-03-26 17:55:43 +0300

    Test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () =>  {
+  SigmaGamesSDK.ShowFullscreenAdv();
   const gridDisplay = document.querySelector('.grid')
   const scoreDisplay = document.getElementById('score')
   const resultDisplay = document.getElementById('result')
